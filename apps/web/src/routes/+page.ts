ae2409efe2f3a913066efe664a1f37f167a366b1
MrPinguiiin <alansnulien@gmail.com>
2026-05-12 02:39:34 +0700

    feat: redirect inbox subdomains to /new in page load function

---
+++ apps/web/src/routes/+page.ts
@@ -0,0 +1,8 @@
+import { env } from "$env/dynamic/public";
+import { redirect } from "@sveltejs/kit";
+
+export const load = ({ url }) => {
+  if (env.PUBLIC_APP_DOMAIN && url.hostname === `inbox.${env.PUBLIC_APP_DOMAIN}`) {
+    redirect(302, "/new");
+  }
+};
